19e7781994c8549d4949244ff567697a238786ec
roger thencera rojas <rothero@gmail.com>
2021-06-01 02:15:35 -0300

    Added Layout and NavBar components, added Semantic-ui library

---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import Head from'next/head';
+import {Navbar} from './Navbar';
+
+ export const Layout=({children})=> {
+  return (
+    <>
+      <Head>
+        <title>Note App</title>
+      </Head>
+      <Navbar/>
+      {children}
+    </>
+  )
+}
+ ;
+
+
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
+import 'semantic-ui-css/semantic.min.css';
+import  {Layout}  from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    )
 }
 
 export default MyApp
